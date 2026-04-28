7aaea1328767e75a19e5214d647ef842ee67213f
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2026-04-28 08:58:46 +0200

    feat: support enum types (#10)

---
+++ test/argument/EnumArgument.spec.js
@@ -0,0 +1,75 @@
+const { createParser, Command } = require('../../src')
+
+it('should accept a valid enum value for an option', async () => {
+  const parser = createParser()
+  parser.addCommand(
+    new Command('paint', 'Paint').withOption({
+      key: 'color',
+      description: 'Color',
+      type: 'enum',
+      enum: ['red', 'green', 'blue'],
+    }),
+  )
+  const context = await parser.parse(['paint', '--color', 'red'])
+  expect(context).toHaveProperty('options')
+  expect(context.options).toHaveProperty('color', 'red')
+})
+
+it('should reject an invalid enum value for an option', async () => {
+  const parser = createParser()
+  parser.addCommand(
+    new Command('paint', 'Paint').withOption({
+      key: 'color',
+      description: 'Color',
+      type: 'enum',
+      enum: ['red', 'green', 'blue'],
+    }),
+  )
+  const context = await parser.parse(['paint', '--color', 'purple'])
+  expect(context).toBe('[color] must be one of: red, green, blue')
+})
+
+it('should accept a valid enum value for a positional argument', async () => {
+  const parser = createParser()
+  parser.addCommand(
+    new Command('paint', 'Paint').withPositional({
+      key: 'color',
+      description: 'Color',
+      type: 'enum',
+      enum: ['red', 'green', 'blue'],
+    }),
+  )
+  const context = await parser.parse(['paint', 'green'])
+  expect(context).toHaveProperty('arguments')
+  expect(context.arguments).toHaveProperty('color', 'green')
+})
+
+it('should reject an invalid enum value for a positional argument', async () => {
+  const parser = createParser()
+  parser.addCommand(
+    new Command('paint', 'Paint').withPositional({
+      key: 'color',
+      description: 'Color',
+      type: 'enum',
+      enum: ['red', 'green', 'blue'],
+    }),
+  )
+  const context = await parser.parse(['paint', 'yellow'])
+  expect(context).toBe('[color] must be one of: red, green, blue')
+})
+
+it('should use the default enum value when no value is provided', async () => {
+  const parser = createParser()
+  parser.addCommand(
+    new Command('paint', 'Paint').withOption({
+      key: 'color',
+      description: 'Color',
+      type: 'enum',
+      enum: ['red', 'green', 'blue'],
+      default: 'blue',
+    }),
+  )
+  const context = await parser.parse(['paint'])
+  expect(context).toHaveProperty('options')
+  expect(context.options).toHaveProperty('color', 'blue')
+})
